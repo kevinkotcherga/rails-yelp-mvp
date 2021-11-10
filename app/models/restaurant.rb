c4d10c2e303a530de1384d66d7ea0e3afca3dc88
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-11-10 16:40:53 +0100

    Yelp mvp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-  validates :name, presence: true, allow_blank: false
-  validates :address, presence: true, allow_blank: false
-  validates :category, presence: true, allow_blank: false, exclusion: { in: %w(neptunian) }
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, presence: true, allow_blank: false
-  validates :rating, presence: true, allow_blank: false, numericality:{ only_integer: true }, :inclusion => 0..5
+  validates :content, presence: true
+  validates :rating, presence: true, numericality:{ only_integer: true }, :inclusion => 0..5
 end
